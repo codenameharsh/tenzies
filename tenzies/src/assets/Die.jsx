a4e6c6c049a6f010b798ed5bac8dfeb6640fb986
codenameharsh <harshitha167@gmail.com>
2025-01-27 21:19:54 -0800

    re-writing for practice

--- tenzies/src/assets/Die.jsx
+++ tenzies/src/assets/Die.jsx
@@ -1,15 +1,25 @@
-export default function Die(props) {
-    const styles = {
-        backgroundColor: props.isHeld ? "#59E391" : "white"
-    }
+// export default function Die(props) {
+//     const styles = {
+//         backgroundColor: props.isHeld ? "#59E391" : "white"
+//     }
     
-    return (
-        <button 
-            style={styles}
-            onClick={props.hold}
-            aria-pressed={props.isHeld}
-            aria-label={`Die with value ${props.value}, 
-            ${props.isHeld ? "held" : "not held"}`}
-        >{props.value}</button>
+//     return (
+//         <button 
+//             style={styles}
+//             onClick={props.hold}
+//             aria-pressed={props.isHeld}
+//             aria-label={`Die with value ${props.value}, 
+//             ${props.isHeld ? "held" : "not held"}`}
+//         >{props.value}</button>
+//     )
+// }
+
+//starting fresh
+
+export default function Die(props) {
+    return(
+        <>
+            <button className="die">{props.value}</button>
+        </>
     )
 }
